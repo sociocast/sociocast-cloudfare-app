19d4befb645846d1a99b7156539590da27659806
Daniel Siegmann <daniel.siegmann@sociocast.com>
2013-08-21 13:54:20 -0400

    [SCP-3232] The protocol used for the URL to the Sociocast javascript is now determined dynamically, taking whatever the user is currently using (http or https).

--- public/javascripts/sociocast.js
+++ public/javascripts/sociocast.js
@@ -5,14 +5,16 @@ CloudFlare.define(
     'sociocast',
     ['sociocast/config'],
     function(config) {
-
         // config will be an object containing any configurations that will be
         // stored by CloudFlare, specific to app + domain combo
-
-        // build the URL for your JS resource
-        var url = 'http://ajs.sociocast.com/'+config.client_id+'/sociocast.js';
-
-        // load and execute file
-        CloudFlare.require([url]);
+        
+        if ( config.client_id && config.client_id != 'UNKNOWN') {
+            // build the URL for the Javascript file which loads Sociocast on the site
+            var protocol = window.location.protocol != "https:" ? "http:" : "https:";
+            var url = protocol+'//ajs.sociocast.com/'+config.client_id+'/sociocast.js';
+            
+            // load and execute file
+            CloudFlare.require([url]);
+        }
     }
 );
